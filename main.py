5b4d019ebb489e0bb8f5c8213f1c598d570072b4
treketerer <andreytop0609@gmail.com>
2026-04-01 22:47:00 +0300

    feat: 2 batch size 3 epoch
    Версия для третьей эпохи обучения

--- main.py
+++ main.py
@@ -16,15 +16,15 @@ from inference import use_model
 # os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 
 # CONFIGS
-BATCH_SIZE = 3
-LEARNING_RATE = 0.0003
-EPOCHS_COUNT = 2
+BATCH_SIZE = 2
+LEARNING_RATE = 0.0001
+EPOCHS_COUNT = 1
 BUFFER_SIZE = 512
 PRINT_COEF = 1
 ACCUMULATION_STEPS = 16
 SCHEDULER_PATIENCE = 5
 
-max_tacts = 10
+max_tacts = 15
 max_token_in_tact = 125
 max_instruments = 13
 
@@ -44,7 +44,7 @@ model_output_path = paths.get("local_models")
 
 NEED_TO_LEARN = False
 LOAD_LEARNED_MODEL = True
-SAVED_MODEL_PATH = f"{model_input_path}/608366_feat_511.pth"
+SAVED_MODEL_PATH = f"{model_input_path}/627340_music_model_1_final.pth"
 
 SOUND_FONT_PATH = "./data/soundfonts/SGM-V2.01.sf2"
 
